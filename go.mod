b36c53fe7454a7603b7a7dc288f74c58f0041eab
Nicolas Zin <nicolas.zin@gmail.com>
2024-08-31 11:39:09 -0400

    initial

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/nzin/taxsi
+
+go 1.22.5
+
+require (
+	github.com/aws/aws-sdk-go v1.44.327 // indirect
+	github.com/go-kit/log v0.2.1 // indirect
+	github.com/go-logfmt/logfmt v0.5.1 // indirect
+	github.com/http-wasm/http-wasm-host-go v0.6.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/rs/zerolog v1.33.0 // indirect
+	github.com/traefik/traefik/v3 v3.1.2 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+)
---
+++ taxsi.go
@@ -0,0 +1,51 @@
+// Package plugindemo a demo plugin.
+package plugindemo
+
+import (
+	"context"
+	"net/http"
+	"os"
+	// "github.com/rs/zerolog/log"
+)
+
+type Rule struct {
+	Rule   string
+	Action string
+}
+
+// Config the plugin configuration.
+type Config struct {
+	RulesFiles []string `json:"RulesFiles"`
+	Rules      []Rule   `json:"Rules"`
+}
+
+// CreateConfig creates the default plugin configuration.
+func CreateConfig() *Config {
+	return &Config{
+		RulesFiles: make([]string, 0),
+		Rules:      make([]Rule, 0),
+	}
+}
+
+// Demo a Demo plugin.
+type Taxsi struct {
+	next http.Handler
+	// headers  map[string]string
+	name string
+}
+
+// New created a new Demo plugin.
+func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
+
+	return &Taxsi{
+		next: next,
+		name: name,
+	}, nil
+}
+
+func (t *Taxsi) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	//log.With().Str("taxsi", t.name)
+	os.Stdout.WriteString("taxsi log")
+
+	t.next.ServeHTTP(rw, req)
+}
